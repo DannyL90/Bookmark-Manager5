665277ed724d25ba606efb1a3c95d82da081ce20
Daniel <daniel.szabo09@gmail.com>
2021-10-14 18:11:01 +0100

    half way to chapter 13

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ class BookmarkManager < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions, :method_override
+
   get "/bookmarks" do
     @bookmarks = Bookmark.all_bookmarks
     erb :"bookmarks/index"
@@ -19,6 +21,12 @@ class BookmarkManager < Sinatra::Base
   post "/bookmarks" do
     Bookmark.create(url: params[:url], title: params[:title])
     redirect "/bookmarks"
+  end
+
+  delete "/bookmarks/:id" do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec_params("DELETE FROM bookmarks WHERE id = $1", [params[:id]])
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
